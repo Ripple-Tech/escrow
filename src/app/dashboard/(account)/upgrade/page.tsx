94e212f96085c29d79809d8efaf0a09e19604df2
Ripple-Tech <ifeanyiinnocentokoro@gmail.com>
2025-08-31 17:50:36 +0100

    uu

--- src/app/dashboard/(account)/upgrade/page.tsx
+++ src/app/dashboard/(account)/upgrade/page.tsx
@@ -1,29 +1,10 @@
-import { DashboardPage } from "@/components/dashboard-page"
-import { db } from "@/db"
-import { currentUser } from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
-import { UpgradePageContent } from "./upgrade-page-content"
 
-const Page = async () => {
-  const auth = await currentUser()
-
-  if (!auth) {
-    redirect("/sign-in")
-  }
-
-  const user = await db.user.findUnique({
-    where: { externalId: auth.id },
-  })
-
-  if (!user) {
-    redirect("/sign-in")
-  }
-
+const page = () => {
   return (
-    <DashboardPage title="Pro Membership">
-      <UpgradePageContent plan={user.plan} />
-    </DashboardPage>
+    <div>
+     Upgrade  Plan
+    </div>
   )
 }
 
-export default Page
+export default page
