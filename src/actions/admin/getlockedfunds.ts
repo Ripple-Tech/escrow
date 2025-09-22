8cc644e0f07ace1ceddea69cebf506cf981205ef
Ripple-Tech <ifeanyiinnocentokoro@gmail.com>
2025-09-22 15:19:49 +0100

    lockfund

--- src/actions/admin/getlockedfunds.ts
+++ src/actions/admin/getlockedfunds.ts
@@ -1,7 +1,7 @@
 import { db } from "@/db"
 
 export const getLockedFunds = async () => {             
-const lockedFunds = await db.lockedFund.findMany({
+const lockedFunds = await db.lockedfund.findMany({
   include: {
     buyer: { select: { email: true } },
     escrow: { select: { id: true, productName: true } },
