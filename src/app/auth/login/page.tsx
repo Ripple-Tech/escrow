e638c65d126126b96c7cf0ce3b73e97536ae1b26
Ripple-Tech <ifeanyiinnocentokoro@gmail.com>
2025-10-08 20:32:58 +0100

    i

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,9 @@ import { LoginForm } from "@/components/auth/login-form";
 
 const LoginPage = () => {
     return (
+        <div className="sm:px-10">
         <LoginForm/> 
+        </div>
     )
 }
 export default LoginPage;
